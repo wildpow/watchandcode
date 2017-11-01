f60700fcdda5e21cef54a4db18724d5d118ff4d9
Aaron Wilder <element1028@gmail.com>
2017-11-01 13:16:20 -0700

    V3

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let todoList = {
+  todos: ['item 1', 'item 2', 'item 3'],
+  displayTodos: function() {
+    console.log('My todos',this.todos)
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+
+};
+todoList.changeTodo(2,2);
+todoList.displayTodos();
+todoList.deleteTodo(2);
