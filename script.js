b2dbe32a1c7649f0259df148f696c8b250ed5c6e
Aaron Wilder <element1028@gmail.com>
2017-11-01 21:46:50 -0700

    v5

--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 let todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My todos',this.todos)
+     if( this.todos.length === 0) {
+        console.log('Your todo list is empty!');
+      } else {
+        for (var i = 0; i < this.todos.length; i++) {
+          if (this.todos[i].completed === true) {
+            console.log('(x)',this.todos[i].todoText);
+          } else {
+            console.log('( )',this.todos[i].todoText);
+          }
+      }
+    }
   },
 
   addTodo: function(todoText) {
@@ -31,10 +41,11 @@ let todoList = {
 
 };
 
-todoList.displayTodos();
 todoList.addTodo('new item');
-todoList.changeTodo(0, 'hello');
-todoList.toggleCompleted(0)
+todoList.addTodo('new item2');
+todoList.displayTodos();
+todoList.toggleCompleted(0);
+
 
 /*
 {
