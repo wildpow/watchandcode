bdc74de52790944b32079c9c78b91fe10d9dab54
Aaron Wilder <element1028@gmail.com>
2017-11-01 23:05:19 -0700

    v7

--- script.js
+++ script.js
@@ -59,3 +59,13 @@ let todoList = {
   }
 
 };
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+})
