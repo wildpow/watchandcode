8a14f99406f87173d72c521f87fdecb3a3b7f69a
Aaron Wilder <element1028@gmail.com>
2017-11-03 12:27:39 -0700

    v10

--- script.js
+++ script.js
@@ -55,10 +55,8 @@ let handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    let deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+    todoList.deleteTodo(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -87,9 +85,29 @@ let view = {
       } else {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
-
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
+      todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
     }
+  },
+  createDeleteButton: function() {
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
+  },
+  setupEventListeners: function() {
+    let todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', function(event) {
+
+      let elementClicked = event.target;
+
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    });
   }
 };
+
+view.setupEventListeners();
